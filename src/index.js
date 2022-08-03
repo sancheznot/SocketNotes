4dac1ae838ea3083d6af43250b2b6485f9c6c641
sancheznot <sancheznotdev@gmail.com>
2022-08-02 22:21:38 -0700

    add new style

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {connectdb} from './database'
 
 connectdb();
 const server = http.createServer(app)
-const httpServer = server.listen(3000);
+const httpServer = server.listen(3500);
 console.log('Server is running on port 3000')
 
 const io = new websocketserver(httpServer)
--- src/public/ui.js
+++ src/public/ui.js
@@ -6,14 +6,29 @@ const notelist = document.querySelector('#notes')
 
 const noteIU = note => {
     const div = document.createElement('div')
-    div.innerHTML =  `
+    div.className = "d-inline-flex p-5 "
+    const div2 = div.appendChild(document.createElement('div'))
+    div2.className = "p-2"
+    div2.innerHTML =  `
         <div class="animate__animated animate__wobble">
-         <h1>${note.title} </h1>
-        <p>${note.description}</p>
+         
+            
+        <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">${note.title}</div>
+        <div class="card-body">
+          <h4 class="card-title">${note.title}</h4>
+          <p class="card-text">${note.description}</p>
+        </div>
+                
+                
+                
+                
+            
         </div> 
+
         <div>
-            <button class="delete" data-id="${note._id}">Delete</button>
-            <button class="edit">Edit</button>
+            <button type="button" class="delete btn btn-danger" data-id="${note._id}">Delete</button>
+            <button class="edit btn btn-info" >Edit</button>
         </div>
         `
     const btndelete = div.querySelector(".delete")
